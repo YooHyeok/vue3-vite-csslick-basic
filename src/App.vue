87778d59e30b739b053eb5f919eed2bd14a1b2da
webdevyoo <yjk7454@naver.com>
2025-02-09 00:56:18 +0900

    feat: `Chapter02` - 데이터 바인딩 > style 속성, 텍스트 노드 바인딩 예제 컴포넌트 추가 및 App 컴포넌트 참조

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Chapter01 from './components/Chapter01.vue'
+import Chapter02 from './components/Chapter02.vue'
 </script>
 
 <template>
   <Chapter01/>
+  <Chapter02/>
 </template>
 
 <style scoped>
