619ad3be5434255009b0d5bb6dcfb3192696f7cd
webdevyoo <yjk7454@naver.com>
2025-02-10 00:12:38 +0900

    feat: `Chapter03` - 상태 변수 데이터 바인딩 ref 변수 추가 및 템플릿 적용

--- src/components/Chapter04.vue
+++ src/components/Chapter04.vue
@@ -1,11 +1,15 @@
 <script setup>
+  import { ref } from 'vue';
   let count = 0;
+  const refCount = ref(0);
   const increment = () => {
     count++;
+    refCount.value++;
     alert(count)
   }
 </script>
 <template>
   <p>count: {{ count }}</p>
+  <p>refCount: {{ refCount }}</p> <!-- 주석시 cnt 반응하지 않음 주석해제시 cnt 함께 반응 -->
   <button @click="increment">Count++</button>
 </template>
