aba9c6e1d08d025bfdc2bc3b0c90db97c18f1170
webdevyoo <yjk7454@naver.com>
2025-02-10 00:13:33 +0900

    feat: `Chapter03` - 상태 변수 데이터 바인딩 ref 변수 바인딩 태그 주석처리 (반응성 추적 OFF)

--- src/components/Chapter04.vue
+++ src/components/Chapter04.vue
@@ -10,6 +10,6 @@
 </script>
 <template>
   <p>count: {{ count }}</p>
-  <p>refCount: {{ refCount }}</p> <!-- 주석시 cnt 반응하지 않음 주석해제시 cnt 함께 반응 -->
+  <!-- <p>refCount: {{ refCount }}</p> --> <!-- 주석시 cnt 반응하지 않음 주석해제시 cnt 함께 반응 -->
   <button @click="increment">Count++</button>
 </template>
