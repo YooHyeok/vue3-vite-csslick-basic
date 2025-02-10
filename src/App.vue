b2d44b2789a110c239fbb8eb2f0c8d3e61f384c3
webdevyoo <yjk7454@naver.com>
2025-02-11 00:33:39 +0900

    feat: `Chapter04_01` - 조건부 랜더링  v-if 디렉티브 예제 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
 import Chapter01 from './components/Chapter01.vue'
 import Chapter02 from './components/Chapter02.vue'
 import Chapter03 from './components/Chapter03.vue'
-import Chapter04_1 from './components/Chapter04_1.vue'
+import Chapter04_01 from './components/Chapter04_01.vue'
+import Chapter04_02 from './components/Chapter04_02.vue'
 </script>
 
 <template>
   <Chapter01/>
   <Chapter02/>
   <Chapter03/>
-  <Chapter04_1/>
+  <Chapter04_01/>
+  <Chapter04_02/>
 </template>
 
 <style scoped>
---
+++ src/components/Chapter04_02.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+const isOpen = ref(true);
+const closeModal = () => {
+  isOpen.value = false
+}
+
+</script>
+<template>
+  <h1>조건부 랜더링(Conditional Redering)</h1>
+  <div class="modal" v-if="isOpen">
+    <h2>Notice</h2>
+    <p>50% off today</p>
+    <button @click="isOpen=false">close</button>
+    <button @click="closeModal">close</button>
+  </div>
+</template>
+<style scoped>
+.modal {
+  background-color: #ccc;
+  padding: 1rem;
+  h2 { margin: 0; color: red; }
+}
+</style>
