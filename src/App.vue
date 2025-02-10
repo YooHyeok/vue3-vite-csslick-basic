755841ec7ce0e3d4165d22b152d371e05a6e0233
webdevyoo <yjk7454@naver.com>
2025-02-11 00:14:53 +0900

    feat: `Chapter04_01` - 반복문 v-for 디렉티브 예제 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 import Chapter01 from './components/Chapter01.vue'
 import Chapter02 from './components/Chapter02.vue'
 import Chapter03 from './components/Chapter03.vue'
-import Chapter04 from './components/Chapter04.vue'
+import Chapter04_1 from './components/Chapter04_1.vue'
 </script>
 
 <template>
   <Chapter01/>
   <Chapter02/>
   <Chapter03/>
-  <Chapter04/>
+  <Chapter04_1/>
 </template>
 
 <style scoped>
---
+++ src/components/Chapter04_01.vue
@@ -0,0 +1,22 @@
+<script setup>
+  const foods = ['apple', 'banana', 'mango']
+  const foodObjs = [
+    { id: 1, name: 'apple' },
+    { id: 2, name: 'banana' },
+    { id: 3, name: 'mango' }
+  ]
+</script>
+<template>
+  <h1>반복문</h1>
+  <ul>
+    <li>{{ foods[0] }}</li>
+    <li>{{ foods[1] }}</li>
+    <li>{{ foods[2] }}</li>
+  </ul>
+  <ul>
+    <li v-for="(food, index) in foods" :key="index">{{ index }}: {{ food }} </li>
+  </ul>
+  <ul>
+    <li v-for="(food) in foodObjs" :key="food.id">{{ food.id }}: {{ food.name }} </li>
+  </ul>
+</template>
