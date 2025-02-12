73be83e15db8ff4d816bc5359a6c0f166d5ce83d
webdevyoo <yjk7454@naver.com>
2025-02-13 00:13:17 +0900

    feat: `Chapter06` -  Event 예제 input 입력 v-model 양방향 구현

--- src/components/Chapter06.vue
+++ src/components/Chapter06.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { ref } from 'vue'
 const count = ref(0);
-const inputValue = ref(''); // 상태변수 추가
-const handleInput = (e) => { // 이벤트 바인딩 함수 추가
+const inputValue = ref('');
+const handleInput = (e) => {
   inputValue.value = e.target.value
 }
+const modelValue = ref(''); // 상태변수 추가
 </script>
 <template>
 <button @click="count++">{{ count }}</button>
 <br>
-<p>{{ inputValue }}</p>
-<input type="text" @input="handleInput" /> <!-- input 태그 추가 및 함수 바인딩 -->
+<p>inputValue: {{ inputValue }}</p>
+<input type="text" @input="handleInput" />
+<br>
+<p>modelValue: {{ modelValue }}</p>
+<input type="text" v-model="modelValue" /> <!-- input 태그 추가 및 v-model 상태변수 바인딩 -->
 </template>
