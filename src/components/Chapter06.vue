09350e9eae74b271d063907263cd4fb5789f68df
webdevyoo <yjk7454@naver.com>
2025-02-13 00:12:51 +0900

    feat: `Chapter06` -  Event 예제 input 입력 단방향 구현

--- src/components/Chapter06.vue
+++ src/components/Chapter06.vue
@@ -1,8 +1,14 @@
 <script setup>
 import { ref } from 'vue'
-const count = ref(0); // 상태변수 추가
-
+const count = ref(0);
+const inputValue = ref(''); // 상태변수 추가
+const handleInput = (e) => { // 이벤트 바인딩 함수 추가
+  inputValue.value = e.target.value
+}
 </script>
 <template>
-<button @click="count++">{{ count }}</button> <!-- button태그 추가 및 실행문 바인딩 -->
+<button @click="count++">{{ count }}</button>
+<br>
+<p>{{ inputValue }}</p>
+<input type="text" @input="handleInput" /> <!-- input 태그 추가 및 함수 바인딩 -->
 </template>
