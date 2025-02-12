cad33c612d9f853c6e42741945ffca902ab23e6a
webdevyoo <yjk7454@naver.com>
2025-02-13 00:11:40 +0900

    feat: `Chapter06` -  Event 예제 버튼클릭 카운트 증가 구현

--- src/components/Chapter06.vue
+++ src/components/Chapter06.vue
@@ -1,6 +1,8 @@
 <script setup>
-  import { ref } from 'vue'
+import { ref } from 'vue'
+const count = ref(0); // 상태변수 추가
+
 </script>
 <template>
-  <button>count</button>
+<button @click="count++">{{ count }}</button> <!-- button태그 추가 및 실행문 바인딩 -->
 </template>
